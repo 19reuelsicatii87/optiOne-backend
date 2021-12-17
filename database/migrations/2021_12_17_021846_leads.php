8f28879ee0e02df81c3d4b76cd83cdfd7b65f124
Reuel Sicat II <reuel@truelogic.com.ph>
2021-12-17 10:20:25 +0800

    removing column from lead table

---
+++ database/migrations/2021_12_17_021846_leads.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Leads extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leads', function (Blueprint $table) {
+            $table->id();
+
+            // Lead Details
+            // ========================================
+            $table->string('fullname', 100)->nullable();
+            $table->string('email', 100)->nullable();
+            $table->string('mobile', 100)->nullable();
+            $table->string('stage', 100)->nullable();
+            $table->string('status', 100)->nullable();
+            $table->string('message', 10000)->nullable();
+
+            // TimeStamp Details
+            // ========================================
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leads');
+    }
+}
